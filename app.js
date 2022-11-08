7f77bad00935d65d737962475cdfa9fc77c30bf9
David Petryca <david@petryca.com>
2022-11-08 09:54:44 +0100

    hello world

---
+++ app.js
@@ -0,0 +1,15 @@
+(() => {
+
+
+    const paper = Snap('#hero');
+
+    const circle = paper.circle('50%', '50%', 100);
+
+    circle.attr({
+        fill: '#FF0000',
+        strokeWidth: 20,
+        stroke: '#0000FF' 
+    });
+
+
+})();
