cd8b911042827b04ef0816b76318e6b0975d220b
David Petryca <david@petryca.com>
2022-11-08 10:33:58 +0100

    final

--- app.js
+++ app.js
@@ -3,13 +3,36 @@
 
     const paper = Snap('#hero');
 
-    const circle = paper.circle('50%', '50%', 100);
+    let i = 0;
 
-    circle.attr({
-        fill: '#FF0000',
-        strokeWidth: 20,
-        stroke: '#0000FF' 
-    });
+
+    function drawCircle() {
+
+        const vw = window.innerWidth;
+        const vh = window.innerHeight;
+
+        i = i + 15;
+        let hue = i % 360;
+
+        let color = Snap.hsl(hue, 100, 40);
+
+        const radius = Math.floor(Math.sqrt((vw ** 2) + (vh ** 2)) / 2) + 150;
+
+        const circle = paper.circle('50%', '50%', 40);
+
+        circle.attr({
+            fill: color
+        });
+
+        circle.animate({ r: radius }, 6000, function () {
+            this.remove();
+        });
+
+    }
+
+    setInterval(drawCircle, 500);
+
+
 
 
 })();
